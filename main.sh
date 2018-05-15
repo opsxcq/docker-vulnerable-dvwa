35c1e50e280c3f0bb8d1c7385bfa351c847345d6
Archis Gore <archis@polyverse.io>
2018-05-15 09:07:43 -0700

    Set Mysql passwords in init.sql

--- main.sh
+++ main.sh
@@ -4,6 +4,21 @@ echo '[+] Starting mysql...'
 echo "--> Ensuring /var/lib/mysql is owned by mysql user."
 chown -R mysql:mysql /var/lib/mysql
 mysql_install_db --user=mysql --ldata=/var/lib/mysql
+
+echo "--> Setting Debian user password for Mysql (in case /var/lib/mysql was mounted)"
+debpass=$(cat /etc/mysql/debian.cnf | grep password | awk '{print $3}' |tail -n 1)
+debpasswdcmd="GRANT ALL PRIVILEGES on *.* TO 'debian-sys-maint'@'localhost' IDENTIFIED BY '$debpass' WITH GRANT OPTION;\nFLUSH PRIVILEGES;"
+
+echo "--> Setting dvwa accessor user for MariaDB"
+dvwacmd="create database dvwa;\ngrant all on dvwa.* to 'dvwa'@'localhost' identified by 'p@ssw0rd';\nflush privileges;"
+
+echo -e "$debpasswdcmd\n$dvwacmd" > /etc/mysql/init.sql
+chmod a+rwx /etc/mysql/init.sql
+
+sed 's/\[mysqld\]/\[mysqld\]\ninit-file = \/etc\/mysql\/init\.sql/' /etc/mysql/my.cnf > /etc/mysql/my.cnf.init
+mv /etc/mysql/my.cnf /etc/mysql/my.cnf.original
+mv /etc/mysql/my.cnf.init /etc/mysql/my.cnf
+
 service mysql start
 echo '--> Verifying mysql service...'
 ss=$(service mysql status)
@@ -21,17 +36,6 @@ if [[ "$ss" == "MySQL is stopped.." ]]; then
 else
 	echo "----> Mysql is running..."
 fi
-
-echo "--> Setting Debian user password for Mysql (in case /var/lib/mysql was mounted)"
-debpass=$(cat /etc/mysql/debian.cnf | grep password | awk '{print $3}' |tail -n 1)
-debpasswdcmd="GRANT ALL PRIVILEGES on *.* TO 'debian-sys-maint'@'localhost' IDENTIFIED BY '$debpass' WITH GRANT OPTION; FLUSH PRIVILEGES;"
-echo "----> $debpasswdcmd"
-mysql -u root -e "$debpasswdcmd"
-
-echo "--> Setting dvwa accessor user for MariaDB"
-dvwacmd="create database dvwa;  grant all on dvwa.* to 'dvwa'@'localhost' identified by 'p@ssw0rd'; flush privileges;"
-echo "----> $dvwacmd"
-mysql -u root -e "$dvwacmd"
 
 echo '[+] Starting apache'
 cp /var/www/html/config/config.inc.php.dist /var/www/html/config/config.inc.php
