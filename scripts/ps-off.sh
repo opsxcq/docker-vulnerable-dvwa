defe275d24e25a5123f05c8d98c408fa9b0a7133
Archis Gore <archis@polyverse.io>
2018-08-01 12:09:56 -0700

    Added scripts to turn ps on/off rapidly

--- scripts/ps-off.sh
+++ scripts/ps-off.sh
@@ -1,5 +1,5 @@
 ln -s /etc/apache2/mods-available/php7.2.load /etc/apache2/mods-enabled
-mv /etc/apache2/mods-enabled/php7.load
+rm /etc/apache2/mods-enabled/php7.load
 
 rm /var/www/html
 ln -s /var/www/html-bak /var/www/html
--- scripts/ps-on.sh
+++ scripts/ps-on.sh
@@ -1,5 +1,5 @@
 ln -s /etc/apache2/mods-available/php7.load /etc/apache2/mods-enabled
-mv /etc/apache2/mods-enabled/php7.2.load
+rm /etc/apache2/mods-enabled/php7.2.load
 
 rm /var/www/html
 ln -s /var/www/html-ps /var/www/html
