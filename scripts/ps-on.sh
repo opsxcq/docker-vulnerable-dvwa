09b1cc7d693e47176dfb8c570d923d25da659116
Archis Gore <archis@polyverse.io>
2018-08-01 12:11:38 -0700

    Create config.inc.php if it doesn't exist after switching contexts

--- scripts/ps-on.sh
+++ scripts/ps-on.sh
@@ -4,4 +4,8 @@ rm /etc/apache2/mods-enabled/php7.2.load
 rm /var/www/html
 ln -s /var/www/html-ps /var/www/html
 
+if [[ ! -f /var/www/html/config/config.inc.php ]]; then
+  cp /var/www/html/config/config.inc.php.dist /var/www/html/config/config.inc.php
+fi
+
 service apache2 restart
