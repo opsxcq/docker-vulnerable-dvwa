590d23096880ffcf320a6dbc1a60d4b68317b2c9
Archis Gore <archis@polyverse.io>
2018-08-01 18:23:01 -0700

    Set mysql users correctly before connecting to it.

--- main.sh
+++ main.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
 
 echo '[+] Starting mysql...'
-echo "--> Ensuring /var/lib/mysql is owned by mysql user."
-chown -R mysql:mysql /var/lib/mysql
-mysql_install_db --user=mysql --ldata=/var/lib/mysql
 
+echo "--> Ensuring /var/lib/mysql is empty and owned by mysql user."
+rm -rf /var/lib/mysql
+mkdir -p /var/lib/mysql
+chown -R mysql:mysql /var/lib/mysql
+
+echo "--> Initializing MySQL..."
+mysqld --initialize-insecure 
+
+echo "--> Starting MySQL..."
+service mysql start
+
+echo "--> Creating usernames and passwords file..."
 echo "--> Setting Debian user password for Mysql (in case /var/lib/mysql was mounted)"
 debpass=$(cat /etc/mysql/debian.cnf | grep password | awk '{print $3}' |tail -n 1)
 debpasswdcmd="GRANT ALL PRIVILEGES on *.* TO 'debian-sys-maint'@'localhost' IDENTIFIED BY '$debpass' WITH GRANT OPTION;\nFLUSH PRIVILEGES;"
@@ -12,14 +21,9 @@ debpasswdcmd="GRANT ALL PRIVILEGES on *.* TO 'debian-sys-maint'@'localhost' IDEN
 echo "--> Setting dvwa accessor user for MariaDB"
 dvwacmd="create database dvwa;\ngrant all on dvwa.* to 'dvwa'@'localhost' identified by 'p@ssw0rd';\nflush privileges;"
 
-echo -e "$debpasswdcmd\n$dvwacmd" > /etc/mysql/init.sql
-chmod a+rwx /etc/mysql/init.sql
+echo -e "$debpasswdcmd\n$dvwacmd" > init.sql
+mysql -u root < init.sql
 
-sed 's/\[mysqld\]/\[mysqld\]\ninit-file = \/etc\/mysql\/init\.sql/' /etc/mysql/my.cnf > /etc/mysql/my.cnf.init
-mv /etc/mysql/my.cnf /etc/mysql/my.cnf.original
-mv /etc/mysql/my.cnf.init /etc/mysql/my.cnf
-
-service mysql start
 echo '--> Verifying mysql service...'
 ss=$(service mysql status)
 echo "$ss"
